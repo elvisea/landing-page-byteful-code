67b90f5557c6528d0a279389054986ac4f36b26a
Elvis <elvis.e.amancio@gmail.com>
2025-02-25 16:22:59 -0300

    footer removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { Contact } from "./components/Contact";
 import { FAQ } from "./components/FAQ";
-import { Footer } from "./components/Footer";
+
 import { Header } from "./components/Header";
 import { Hero } from "./components/Hero";
 import { Process } from "./components/Process";
